5441d7fbc0f97e3cd0524c18103252f80e3310e2
Maksym Bykovskyy <maksym@bykovskyy.com>
2024-07-03 14:33:53 +0100

    Added Dockerfile, trying to get jar work.

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+FROM maven:3.9.7-eclipse-temurin-22-alpine AS build
+ENV APP_DIR=/usr/app
+RUN mkdir -p $APP_DIR
+WORKDIR $APP_DIR
+COPY src src
+COPY pom.xml .
+RUN mvn -f pom.xml clean package
+
+FROM eclipse-temurin:22-jre-alpine
+ARG JAR_FILE=/usr/app/target/spritey-server-*.jar
+COPY --from=build $JAR_FILE /app/server.jar
+EXPOSE 8080
+ENTRYPOINT java -jar /app/server.jar
--- src/main/kotlin/com/bykovskyy/Application.kt
+++ src/main/kotlin/com/bykovskyy/Application.kt
@@ -1,6 +1,7 @@
 package com.bykovskyy
 
 import com.fasterxml.jackson.databind.SerializationFeature
+import io.ktor.http.content.*
 import io.ktor.serialization.jackson.jackson
 import io.ktor.server.application.Application
 import io.ktor.server.application.call
@@ -8,13 +9,20 @@ import io.ktor.server.application.install
 import io.ktor.server.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.server.routing.routing
 import io.ktor.server.routing.get
+import io.ktor.server.routing.post
 import io.ktor.server.response.respondText
 import io.ktor.server.response.respond
+import io.ktor.server.request.receiveMultipart
 import io.ktor.server.netty.EngineMain
+import io.ktor.server.plugins.cors.routing.CORS
+import java.io.File
 
 fun main(args: Array<String>): Unit = EngineMain.main(args)
 
 fun Application.module() {
+    install(CORS) {
+        allowHost("localhost:3000")
+    }
     install(ContentNegotiation) {
         jackson {
             enable(SerializationFeature.INDENT_OUTPUT)
@@ -26,6 +34,26 @@ fun Application.module() {
         }
         get("/json/jackson") {
             call.respond(mapOf("hello" to "world"))
+        }
+        post("/files") {
+            val multipartData = call.receiveMultipart()
+
+            multipartData.forEachPart { part ->
+                when (part) {
+                    is PartData.FormItem -> {
+                        val fileDescription = part.value
+                    }
+                    is PartData.FileItem -> {
+                        val filename = part.originalFileName as String
+                        val fileBytes = part.streamProvider().readBytes()
+                        File("uploads/$filename").writeBytes(fileBytes)
+                    }
+                    else -> {}
+                }
+                part.dispose()
+            }
+
+            call.respondText("Hello world!")
         }
     }
 }
